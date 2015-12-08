d6c918f1c20a8dfd42f02dd1cac76bc66cac7ca3
reimu <r2hkri@gmail.com>
2015-12-07 18:35:52 -0600

    christmas update!

--- script.js
+++ script.js
@@ -1,17 +1,107 @@
 // ==UserScript==
 // @name         github GMOD Piano Script
 // @namespace    http://your.homepage/
-// @version      1.04
+// @version      1.05
 // @description  enter something useful
 // @author       You
 // @match        http://www.multiplayerpiano.com/*
 // @grant        none
 // ==/UserScript==
-
+/*------------------ CUSTOM SNOW -----------------*/
 $(function() {
 
     /*-------------- BEGIN CUSTOM --------------*/
 
+    // christmas snow!!
+    //////////////////////////////////
+    //canvas init
+     $("body").prepend('<canvas id="canvassnow"></canvas>');
+    //canvas init
+	var canvas = document.getElementById("canvassnow");
+	var ctx = canvas.getContext("2d");
+	
+	//canvas dimensions
+	var W = window.innerWidth;
+    var Hb = window.innerHeight;
+	var H = window.innerHeight;
+	canvas.width = W;
+	canvas.height = H;
+	
+	//snowflake particles
+	var mp = 50; //max particles
+	var particles = [];
+	for(var i = 0; i < mp; i++)
+	{
+		particles.push({
+			x: Math.random()*W, //x-coordinate
+			y: Math.random()*H, //y-coordinate
+			r: Math.random()*4+1, //radius
+			d: Math.random()*mp //density
+		})
+	}
+	
+	//Lets draw the flakes
+	function draw()
+	{
+		ctx.clearRect(0, 0, W, H);
+		
+		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+		ctx.beginPath();
+		for(var i = 0; i < mp; i++)
+		{
+			var p = particles[i];
+			ctx.moveTo(p.x, p.y);
+			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+		}
+		ctx.fill();
+		update();
+	}
+	
+	//Function to move the snowflakes
+	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+	var angle = 0;
+	function update()
+	{
+		angle += 0.01;
+		for(var i = 0; i < mp; i++)
+		{
+			var p = particles[i];
+			//Updating X and Y coordinates
+			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+			//Every particle has its own density which can be used to make the downward movement different for each flake
+			//Lets make it more random by adding in the radius
+			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+			p.x += Math.sin(angle) * 2;
+			
+			//Sending flakes back from the top when it exits
+			//Lets make it a bit more organic and let flakes enter from the left and right also.
+			if(p.x > W+5 || p.x < -5 || p.y > H)
+			{
+				if(i%3 > 0) //66.67% of the flakes
+				{
+					particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
+				}
+				else
+				{
+					//If the flake is exitting from the right
+					if(Math.sin(angle) > 0)
+					{
+						//Enter from the left
+						particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+					}
+					else
+					{
+						//Enter from the right
+						particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+					}
+				}
+			}
+		}}
+	
+	
+	//animation loop
+	var snowloop = setInterval(draw, 33);
+    
     // sound select
     //////////////////////////////////
     // select sound before anything
@@ -31,6 +121,8 @@ $(function() {
     var style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = '#lol{display:none;}#console2{font-size:15px;color:#ffffff;position:absolute;top:10px;right:350px;width:500px;height:90px;display:block;overflow:hidden;background:#000000;;border:1px solid white;}#panel{font-size:15px;position:absolute;top:10px;right:100px;width:200px;height:250px;display:block;padding:0px 10px 10px 10px;background:#fff;z-index:250;border:1px solid black;float: top;color:#000;}';
+    //christmasy
+    style.innerHTML += '.expand{background:#fff}#room{background:#fff}.relative{background:#fff}body{background: #6b92b9;}canvassnow{display:block;z-index:-1;}'
     document.getElementsByTagName('head')[0].appendChild(style);
 
     // this versions number
@@ -510,7 +602,8 @@ $(function() {
     Renderer.prototype.resize = function(width, height) {
         if(typeof width == "undefined") width = $(this.piano.rootElement).width();
         if(typeof height == "undefined") height = Math.floor(width * 0.2);
-        $(this.piano.rootElement).css({"height": height + "px", marginTop: Math.floor($(window).height() / 2 - height / 2) + "px"});
+        //custom: - (H) to be used with snow overlay
+        $(this.piano.rootElement).css({"height": height + "px", marginTop: Math.floor($(window).height() / 2 - height / 2) - (H) + "px"});
         this.width = width;
         this.height = height;
     };
